5b601fe5c6d4f4116407c0ad6d29ce08dc4a00e9
andres <locogb26@gmail.com>
2025-04-01 15:38:12 -0500

    agregando endpoint post users

--- app/main.py
+++ app/main.py
@@ -21,5 +21,23 @@ def get_users():
     finally:
         conn.close()
 
+
+@app.route("/users", methods=["POST"])
+def create_user():
+    try:
+        data = request.json
+        user = User(data['name'], data['email'])
+
+        conn = get_connection()
+        with conn.cursor() as cursor:
+            sql = "INSERT INTO users(name, email) VALUES (%s, %s);"
+            cursor.execute(sql, (user.name, user.email))
+            conn.commit()
+        return jsonify({"message": "Usuario creado con exito"}), 201
+    except Exception as e:
+        return jsonify({"message": str(e)})
+    finally:
+        conn.close()
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
