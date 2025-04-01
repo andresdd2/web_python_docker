1a1920d993fe9b9f9f2a6765a9f29803232b86e7
andres <locogb26@gmail.com>
2025-04-01 15:18:00 -0500

    endpoint de obtener los users de la tabla users

--- app/main.py
+++ app/main.py
@@ -8,5 +8,18 @@ app = Flask(__name__)
 def home():
     return jsonify({"message": "Api Flask con Python"})
 
+@app.route('/users', methods=["GET"])
+def get_users():
+    try:
+        conn = get_connection()
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM users")
+            user = cursor.fetchall()
+            return jsonify({"users": user})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        conn.close()
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
